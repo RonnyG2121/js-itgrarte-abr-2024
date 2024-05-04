b96acb7753f7bda7560680567200d8be6fe96067
Ronny Omnier Gonzalez Vargas <yordemblo@gmail.com>
2024-05-04 19:27:17 +0200

    Añadidos enlaces del código y primera tarea

---
+++ codigos/tema1/intro.js
@@ -0,0 +1,30 @@
+// Este es un comentario de una línea
+
+/*
+Hola
+Esto es un comentario
+Multilínea
+
+ */
+
+// Algoritmo que sume 2 números
+
+// Entrada
+let num1 = 15;
+let num2 = 20;
+// Proceso
+let resultado = num1 + num2;
+
+// Salida
+console.log(resultado);
+
+// uso de una constante
+const CONSTANTE1 = 3.14;
+
+// Mostrando la constante por pantalla
+console.log(CONSTANTE1);
+
+// Lo siguiente no se puede hacer, y por ende estará comentado
+// CONSTANTE1 = 3.16;
+
+console.log(CONSTANTE1);
