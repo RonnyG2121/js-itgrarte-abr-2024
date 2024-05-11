0dd885f3baa2ff15c6ddf35055a6287f6f6ffe90
Ronny Omnier Gonzalez Vargas <yordemblo@gmail.com>
2024-05-11 22:54:57 +0200

    Añadidos código y tarea del tema 2

---
+++ codigos/tema2/operadores_y_tipos.js
@@ -0,0 +1,41 @@
+// Uso del operador de suma y el tipo de dato numérico
+
+let num1 = 15;
+
+let num2 = 10;
+
+console.log(num1 + num2);
+
+// Uso del operador de resta
+let resultado_resta = num1 - num2;
+console.log(resultado_resta);
+
+// uso del operador de resto o módulo
+console.log(num1 % num2);
+
+
+// Raíz cuadrada
+let raiz = Math.sqrt(25);
+
+// Uso de los string o cadenas
+let nombre = "Ronny";
+let apellido = 'González';
+let resultado_edad = `Mi edad es: ${num1}`;
+
+console.log("Hola mi nombre es: " + nombre + apellido + resultado_edad);
+
+// Uso de cadenas y números
+let suma_cadena = 5;
+let valor_cadena = "5" + suma_cadena;
+
+console.log(valor_cadena);
+
+// Uso del operador mayor y menor que
+console.log(num1 > num2);
+
+console.log(num1 < num2);
+
+// Uso de los arrays
+let arleglo = ["juan", 25, {}, true];
+console.log(arreglo);
+
